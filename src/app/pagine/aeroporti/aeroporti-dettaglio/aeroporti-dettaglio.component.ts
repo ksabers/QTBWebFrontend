7555f42f4448a118885916b6fff4bff8f3948f5f
Giorgio Borgo <giorgio.borgo@algowatt.com>
2021-12-03 18:03:51 +0100

    struttura a tab sul dettaglio aeroporti

--- src/app/pagine/aeroporti/aeroporti-dettaglio/aeroporti-dettaglio.component.ts
+++ src/app/pagine/aeroporti/aeroporti-dettaglio/aeroporti-dettaglio.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Aeroporto } from 'src/app/viewmodels/aeroporto';
 
 @Component({
   selector: 'app-aeroporti-dettaglio',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AeroportiDettaglioComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(MAT_DIALOG_DATA) public aeroporto: Aeroporto) { }
 
   ngOnInit(): void {
   }
