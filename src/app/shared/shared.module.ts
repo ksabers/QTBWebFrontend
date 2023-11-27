2611316f6e78f7f69d4e74ebbaf67d5ae5b25882
Giorgio Borgo <giorgio.borgo@gmail.com>
2023-11-27 15:55:33 +0100

    installato nuovo fxlayout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { TranslateModule } from '@ngx-translate/core';
 import { AngularMaterialModule } from './../angular-material/angular-material.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { FlexLayoutModule } from '@ngbracket/ngx-layout';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 
